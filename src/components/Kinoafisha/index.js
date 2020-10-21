b504045324ab7ee8e7ace87835117cbcd42368b3
Andrey Prisniak <aprisniak@lectrum.io>
2020-10-21 22:06:36 +0300

    feat(source): initial commit

---
+++ src/components/Kinoafisha/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../theme/init.css';
+
+export const Kinoafisha = () => {
+    return (
+        <section className = 'welcome'>
+            <h1>Добро пожаловать!</h1>
+        </section>
+    );
+};
